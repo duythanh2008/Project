7e7d186bde17187805561af79fb5d61ea03b88d6
moskilllllmht <thanhnguyen15022008@gmail.com>
2021-08-07 22:22:39 +0700

    Calc profit

--- Controller/C-trangquantri.php
+++ Controller/C-trangquantri.php
@@ -5,5 +5,14 @@
     else{
         header('location: ?controller=dangnhapquantri');
     }
+    $user_order = $database->get('user_order',array());
+    $user_order_detail = $database->get('user_order_detail',array());
+    $user_order_not_confirmed = $database->get('user_order',array('status'=>0));
+    $user_order_confirmed = $database->get('user_order',array('status'=>1));
+    foreach ($user_order_not_confirmed as $key => $value){
+        foreach ($database->get('user_order_detail',array('order_id'=>$value['id'])) as $key => $values){
+            
+        }
+    }
     require_once './View/V-trangquantri.php';
 ?>
--- View/V-trangquantri.php
+++ View/V-trangquantri.php
@@ -98,6 +98,22 @@
                         </ul>
                     </div>
                 </div>
+                <?php 
+                    $profit = 0;
+                    foreach ($user_order_confirmed as $key => $value){
+                        $profit += $value['amount'];
+                    }
+                    $all_product = 0;
+                    foreach ($user_order_detail as $key => $value){
+                        $all_product += $value['qty'];
+                    }
+                    $ex_product = 0;
+                    foreach ($user_order_not_confirmed as $key => $value){
+                        foreach ($database->get('user_order_detail',array('order_id'=>$value['id'])) as $key => $values){
+                            $ex_product += $values['qty'];
+                        }
+                    }
+                ?>
                 <div class="admin-content">
                     <div class="row no-gutters">
                         <div class="col l-12 m-12 c-12">
@@ -108,7 +124,7 @@
                                 <i class="fas fa-money-bill-wave-alt admin-content__profit-img"></i>
                                 <div class="admin-content__profit-content">
                                     <h3 class="admin-content__profit-heading">Doanh thu cửa hàng tháng này</h3>
-                                    <h4 class="admin-content__profit-info">7.950.000đ</h4>
+                                    <h4 class="admin-content__profit-info"><?php echo number_format($profit) ?>đ</h4>
                                 </div>
                             </div>
                         </div>
@@ -117,7 +133,7 @@
                             <i class="fas fa-box admin-content__all-item-img"></i>
                                 <div class="admin-content__all-item-content">
                                     <h3 class="admin-content__all-item-heading">Tổng đơn hàng tháng này</h3>
-                                    <h4 class="admin-content__all-item-info">122</h4>
+                                    <h4 class="admin-content__all-item-info"><?php echo count($user_order) ?></h4>
                                 </div>
                             </div>
                         </div>
@@ -126,7 +142,7 @@
                             <i class="fas fa-coins admin-content__item-sold-img"></i>
                                 <div class="admin-content__item-sold-content">
                                     <h3 class="admin-content__item-sold-heading">Sản phẩm đã bán tháng này</h3>
-                                    <h4 class="admin-content__item-sold-info">102</h4>
+                                    <h4 class="admin-content__item-sold-info"><?php echo $all_product - $ex_product ?></h4>
                                 </div>
                             </div>
                         </div>
@@ -135,7 +151,7 @@
                             <i class="fas fa-hand-paper admin-content__item-not-confirm-img"></i>
                                 <div class="admin-content__item-not-confirm-content">
                                     <h3 class="admin-content__item-not-confirm-heading">Đơn hàng chưa xác nhận</h3>
-                                    <h4 class="admin-content__item-not-confirm-info">20</h4>
+                                    <h4 class="admin-content__item-not-confirm-info"><?php echo count($user_order_not_confirmed) ?></h4>
                                 </div>
                             </div>
                         </div>
