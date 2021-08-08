30ca5878b747294fecd26c704eb9201b78cf5faa
moskilllllmht <thanhnguyen15022008@gmail.com>
2021-08-08 11:01:27 +0700

    fix bug home page

--- Controller/C-trangchu.php
+++ Controller/C-trangchu.php
@@ -6,12 +6,10 @@
         $keyword = $_GET['keyword'];
         $product = $database->get_like('product','name',$keyword);
     }
-    else{
         $product1 = $database->get_limit('product',array('catalog'=>2),5);
         $product2 = $database->get_limit('product',array('catalog'=>3),5);
         $product3 = $database->get_limit('product',array('catalog'=>4),5);
         $product4 = $database->get_limit('product',array('catalog'=>1),5);
         $product5 = $database->get_limit('product',array('catalog'=>5),5);
-    }
     require_once './View/V-trangchu.php';
 ?>
